4f5dc2dd04a283d58f0896112dc205fe66e0513d
cashcoder88 <cashglobe@gmail.com>
2019-04-29 14:12:27 -0600

    Passed posts as prop for PostContainer

--- instagram-app/src/App.js
+++ instagram-app/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import dummyData from './dummy-data';
 import './App.css';
+import PostContainer from './Components/PostContainer/PostContainer';
 
 class App extends Component {
   constructor() {
@@ -12,7 +13,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        Testing 123
+        <PostContainer posts={this.state.posts} />
       </div>
     );
   }
