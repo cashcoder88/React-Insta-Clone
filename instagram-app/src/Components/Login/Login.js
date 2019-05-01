a3a22daf227b00b7017d3454bcadba3a787c9821
cashcoder88 <cashglobe@gmail.com>
2019-05-01 15:51:12 -0600

    Finished login page

--- instagram-app/src/Components/Login/Login.js
+++ instagram-app/src/Components/Login/Login.js
@@ -14,25 +14,38 @@ class Login extends React.Component {
         this.setState({ [e.target.name]: e.target.value});
     }
 
-
+    handleLogin = e => {
+        localStorage.setItem('user', this.state.username)
+        window.location.reload();
+    }
 
 
     render() {
         return (
             <div className="login-wrapper">
+                <div className="welcome">
+                    <h1>Welcome To My Instagram Clone</h1>
+                    <h2>Please Login With Your Username and Password</h2>
+                </div>
                 <div className="username-input">
                     <input 
                     type="text" 
                     placeholder="Username"
+                    name="username"
+                    value={this.state.username}
+                    onChange={this.handleInput}
                     />
                 </div>
                 <div className="password-input">
                     <input 
                     type="text" 
                     placeholder="Password"
+                    name="password"
+                    value={this.state.password}
+                    onChange={this.handleInput}
                     />
                 </div>
-                <button className="button">Login</button>
+                <button className="button" onClick={this.handleLogin}>Login</button>
             </div>
         )
     }
