578bdc31d191c269bf93713ca3f25bf04cdbf1bd
cashcoder88 <cashglobe@gmail.com>
2019-05-01 15:40:41 -0600

    Changed Login to Class comp for state, and functions

--- instagram-app/src/Components/Login/Login.js
+++ instagram-app/src/Components/Login/Login.js
@@ -1,23 +1,34 @@
 import React from 'react';
 
-const Login = () => {
-    return (
-        <div className="login-wrapper">
-            <div className="username-input">
-                <input 
-                type="text" 
-                placeholder="Username"
-                />
+class Login extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            username: '',
+            password: ''
+        }
+    }
+
+    render() {
+        return (
+            <div className="login-wrapper">
+                <div className="username-input">
+                    <input 
+                    type="text" 
+                    placeholder="Username"
+                    />
+                </div>
+                <div className="password-input">
+                    <input 
+                    type="text" 
+                    placeholder="Password"
+                    />
+                </div>
+                <button className="button">Login</button>
             </div>
-            <div className="password-input">
-                <input 
-                type="text" 
-                placeholder="Password"
-                />
-            </div>
-            <button className="button">Login</button>
-        </div>
-    )
+        )
+    }
+
 }
 
 export default Login;
