f46484699f7058b951f659529e23a7a259b8c2bc
cashcoder88 <cashglobe@gmail.com>
2019-05-01 15:16:04 -0600

    Created withAuthenticate HOC

---
+++ instagram-app/src/Components/Authentication/withAuthenticate.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const withAuthenticate = PassedInComp => class extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return (
+
+        )
+    }
+}
+
+export default withAuthenticate;
